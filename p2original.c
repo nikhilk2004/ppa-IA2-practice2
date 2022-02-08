aac518ede0602ed851233a1ca6a9cf5b0d2fd849
nikhil2004 <93114389+nikhilk2004@users.noreply.github.com>
2022-02-08 06:34:32 +0000

    complete

--- p2original.c
+++ p2original.c
@@ -1,5 +1,32 @@
 #include<stdio.h>
 int input_side()
 {
-  
+  int n;
+  printf("enter any number :");
+  scanf("%d",&n);
+  return n;
+}
+int check_scalene(int a,int b,int c)
+{
+  if( a!= b && b!=c)
+    return 0;
+  else
+    return 1;
+}
+void output(int a,int b,int c,int scalene)
+{
+  if(scalene==0)
+  printf("SCALENE");
+  else
+  printf("NOT SCALENE");
+} 
+int main()
+{
+  int a,b,c,d;
+  a=input_side();
+  b=input_side();
+  c=input_side();
+  d=check_scalene(a,b,c);
+  output(a,b,c,d);
+  return 0;
 }

6d3b1426d84b9ef0d8dc450b46423fc6c4c8e00a
nikhil2004 <93114389+nikhilk2004@users.noreply.github.com>
2022-03-15 06:30:45 +0000

    compeletd

---
+++ p6original.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void input_string(char *a)
+{
+  printf("enter the string :");
+  scanf("%s",a);
+}
+int str_reverse(char *a)
+{
+  int i,m=0;
+  for(i=0;a[i]!='\0';i++)
+  {
+    m++;
+  }
+  return m;
+}
+void output(char *a,char reversea)
+{
+  printf("the reversea of thr string is :");
+  for(int i=reversea-1;a[i]!='\0';i--)
+    {
+      printf("%c",a[i]);
+    }
+}
+int main()
+{
+  int reverse_a;
+  char a[100];
+  input_string(a);
+  reverse_a=str_reverse(a);
+  output(a,reverse_a);
+  return 0;
+}
